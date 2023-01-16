baf86d423bf26c759c80cb316762cc2b520994c1
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2023-01-16 16:19:33 -0400

    Adds org roam basic template

--- org.el
+++ org.el
@@ -9,7 +9,6 @@
 (setq! org-roam-directory (file-truename "~/org/"))
 (setq! org-roam-dailies-directory "daily/")
 
-
 ;; Org superstar
 (setq! org-superstar-special-todo-items nil)
 (add-hook! 'org-mode-hook (lambda ()
@@ -19,3 +18,9 @@
 (org-babel-do-load-languages
  'org-babel-load-languages
  '((js . t)))
+
+;; Roam Capture Templates
+(setq! org-roam-capture-templates
+      '(("d" "default" plain "%?"
+         :target (file+head "%<%Y%m%d_%H%M>-${slug}.org" "#+title: ${title}\n")
+         :unnarrowed t)))
