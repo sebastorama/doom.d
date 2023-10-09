9c02fb8b051c9cde2e6027eef9932000553b4b96
Sebastiao G Ferreira, Jr <sebastorama@gmail.com>
2023-10-09 19:56:00 -0300

    Apheleia format json with respecting .prettierrc

--- lsp.el
+++ lsp.el
@@ -5,3 +5,8 @@
 
 (add-hook! 'typescript-ts-mode-hook (lsp))
 (add-hook! 'tsx-ts-mode-hook (lsp))
+
+;; customize apheleia formatter for prettier-json
+(after! apheleia
+  (setf (alist-get 'prettier-json apheleia-formatters)
+        '("prettier" "--parser" "json")))
